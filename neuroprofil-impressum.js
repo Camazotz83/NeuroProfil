1a61d918e9aa3a80133bfc2bc82d5c225ded10dd
Camazotz <wahmhoff@gmail.com>
2026-09-10 06:26:45 +0200

    Add files via upload

---
+++ neuroprofil-impressum.js
@@ -0,0 +1,10 @@
+window.NP_CONFIG = {
+  version: "3.0.0",
+  edition: "Clinical Matrix",
+  paypal: "https://www.paypal.com/paypalme/wahmhoff83",
+  imprint: {
+    name: "Sascha Wahmhoff",
+    address: "[Tannenburgstrasse 76 in 49084 OS",
+    email: "wahmhoff@gmsil.com"
+  }
+};
